e73d34002b29e7adb54c7e8a770722743290f367
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-08-06 23:26:04 +0300

    SOLID - resourses

---
+++ 04 - Java OOP/src/OOP06_SOLID/solidLab/p01_SingleResponsibility/p01_DrawingShape/interfaces/Renderer.java
@@ -0,0 +1,5 @@
+package OOP06_SOLID.solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces;
+
+public interface Renderer {
+    void render(solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.DrawingRepository drawingRepository, Shape shape);
+}
